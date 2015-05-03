2799ad84dc79867ba20c7dfba92bd92bec036a42
raton023 <volaras23@gmail.com>
2015-05-02 22:27:46 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,8 +11,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.material.MaterialData;
+import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -20,7 +22,7 @@ public class Main extends JavaPlugin implements Listener {
 	public void onEnable() {
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	}
-	ArrayList<MaterialData> potions = new ArrayList<MaterialData>();
+	ArrayList<String> potions = new ArrayList<String>();
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void clickpotion(PlayerInteractEvent e){
@@ -36,23 +38,137 @@ public class Main extends JavaPlugin implements Listener {
 	    		 int a = b.getData();
 		    		 if(a <= 3){a++;
 		    		 b.setData((byte) a);
-		    		 potions.add(p.getItemInHand().getData());
+		    		 potions.add(p.getItemInHand().getData().toString().replace("POTION(", "").replace(")", ""));
 }
 		    		 if(potions.size() == 3){
-		    			 //Object[] pociones = potions.toArray();
-		    			 p.sendMessage("you got 3 " + potions.get(0).toString().replace("POTION(", "").replace(")", ""));
-		    			 short poo = 0;
-		    			 if(potions.get(0).toString().endsWith("1)")){
-		    			 poo = 1;
+		    			 p.sendMessage("you got 3 " + potions.get(0) + " " + potions.get(1) + " " + potions.get(2));
+		    			 
+		    			 ItemStack potion = new ItemStack(Material.POTION);
+		    			 PotionMeta meta = (PotionMeta) potion.getItemMeta();
+
+		    			 
+		    			 if(potions.get(0).contentEquals("35")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 600, 1), true);
 		    			 }
+		    			 if(potions.get(0).contentEquals("76")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.HARM, 600, 1), true);
+		    			 }
+		    			 if(potions.get(0).contentEquals("69")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.HEAL, 600, 1), true);
+		    			 }
+		    			 if(potions.get(0).contentEquals("9")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 600, 1), true);
+		    			 }
+		    			 if(potions.get(0).contentEquals("46")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 600, 1), true);
+		    			 }
+		    			 if(potions.get(0).contentEquals("38")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 600, 1), true);
+		    			 }
+		    			 if(potions.get(0).contentEquals("4")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.POISON, 600, 1), true);
+		    			 }
+		    			 if(potions.get(0).contentEquals("1")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.REGENERATION, 600, 1), true);
+		    			 }
+		    			 if(potions.get(0).contentEquals("42")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.SLOW, 600, 1), true);
+		    			 }
+		    			 if(potions.get(0).contentEquals("2")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.SPEED, 600, 1), true);
+		    			 }
+		    			 if(potions.get(0).contentEquals("45")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 600, 1), true);
+		    			 }
+		    			 if(potions.get(0).contentEquals("40")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.WEAKNESS, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("35")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("76")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.HARM, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("69")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.HEAL, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("9")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("46")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("38")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("4")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.POISON, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("1")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.REGENERATION, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("42")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.SLOW, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("2")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.SPEED, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("45")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 600, 1), true);
+		    			 }
+		    			 if(potions.get(1).contentEquals("40")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.WEAKNESS, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("35")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("76")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.HARM, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("69")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.HEAL, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("9")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("46")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("38")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("4")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.POISON, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("1")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.REGENERATION, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("42")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.SLOW, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("2")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.SPEED, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("45")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 600, 1), true);
+		    			 }
+		    			 if(potions.get(2).contentEquals("40")){
+				    		 meta.addCustomEffect(new PotionEffect(PotionEffectType.WEAKNESS, 600, 1), true);
+		    			 }
+		    			 
+			    		 
+			    		 potion.setItemMeta(meta);
+			    		 p.getInventory().addItem(potion);
+
+		    			 
 		    			 potions.remove(0);
 		    			 potions.remove(0);
 		    			 potions.remove(0);
 		    			 
 		    			 a=0;
 			    		 b.setData((byte) 0);
-			    		 ItemStack pota = new ItemStack(Material.POTION, 1, (short) poo);
-			    		 p.getWorld().dropItemNaturally(p.getLocation(), pota);
+			    		 //ItemStack pota = new ItemStack(Material.POTION, 1, (short) 6); // el 6 es el id del effecto
+			    		 //p.getWorld().dropItemNaturally(p.getLocation(), pota);
 		    		 }
 		    		 
 	 	    	 }
